e7d4f98a752c17585d4377d64e5f6dd22fc19895
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2025-03-20 10:41:44 +0530

    Update Reducers.js

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -32,7 +32,7 @@ export const productReducer = (state, action) => {
     case "FILTER_BY_SEARCH":
       return { ...state, searchQuery: action.payload.toLowerCase().trim() };
     case "CLEAR_FILTERS":
-      return { byStock: false, byFastDelivery: false, byRating: 0 };
+      return { byStock: false, sort: "lowToHigh", byFastDelivery: false, byRating: 0 };
     default:
       return state;
   }
